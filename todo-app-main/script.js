7eb5e40e4075a7dfa470247b56ee9c62cf3603ea
Muhammad Bilal <beastop975@gmail.com>
2023-09-07 17:35:23 +0500

    Recreating

---
+++ todo-app-main/script.js
@@ -0,0 +1,27 @@
+const input = document.querySelector('.input');
+const ul = document.querySelector('ul');
+const todoList = document.querySelector('.todo-list');
+const addBtn = document.querySelector('.addBtn')
+
+input.addEventListener('keypress', (e)=> {
+    if (e.key === 'Enter') {
+      if (input.value === '') {
+        alert('You must add a todo')
+      }
+      else{
+  e.preventDefault();
+  let inputText = input.value;
+
+  const li = document.createElement('li');
+  const checkBox = document.createElement('input');
+  checkBox.type = 'checkbox';
+  checkBox.classList.add('checkBox');
+  const p = document.createElement('p');
+  p.innerHTML = inputText;
+  li.appendChild(checkBox);
+  li.appendChild(p);
+
+  todoList.appendChild(li)
+    }
+  }
+})
