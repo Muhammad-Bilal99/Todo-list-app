70143bb59b21e7551d1a4ef6368ab681721dfa29
Muhammad Bilal <beastop975@gmail.com>
2023-09-29 15:44:23 +0500

    Made delete button and items left work

--- todo-app-main/script.js
+++ todo-app-main/script.js
@@ -1,12 +1,14 @@
 const input = document.querySelector('.input');
 const ul = document.querySelector('ul');
 const todoList = document.querySelector('.todo-list');
-const addBtn = document.querySelector('.addBtn')
+const addBtn = document.querySelector('.addBtn');
+const itemsLeft = document.querySelector('.no-of-items');
 
+let todos = [];
 input.addEventListener('keypress', (e)=> {
     if (e.key === 'Enter') {
       if (input.value === '') {
-        alert('You must add a todo')
+        alert('You must add a todo');
       }
       else{
   e.preventDefault();
@@ -16,14 +18,29 @@ input.addEventListener('keypress', (e)=> {
   const checkBox = document.createElement('input');
   checkBox.type = 'checkbox';
   checkBox.classList.add('checkBox');
+  checkBox.addEventListener('click',() => {
+    checkBox.classList.add('checkBox-active')
+  })
   const p = document.createElement('p');
   p.innerHTML = inputText;
+
+  const deleteBtn = document.createElement('button')
+  deleteBtn.classList.add('deleteBtn');
+  deleteBtn.addEventListener('click', () => {
+    li.remove();
+    todos = todos.filter(todo => todo !== li.textContent);
+    itemsLeft.innerHTML = `${todos.length} ${todos.length === 1 ? 'item' : 'items'} left`;
+  });
+
   li.appendChild(checkBox);
   li.appendChild(p);
+  li.appendChild(deleteBtn);  
 
+  todos.push(li.textContent);
+  itemsLeft.innerHTML = `${todos.length} ${todos.length === 1 ? 'item' : 'items'} left`;
 
-  todoList.appendChild(li)
-  input.value = ''
-}
+  ul.appendChild(li);
+  input.value = '';
+  }
 }
 })
