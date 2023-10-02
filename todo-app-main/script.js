c55a3b9a5da6ef7c97f5151083feb698e08395c1
Muhammad Bilal <beastop975@gmail.com>
2023-10-02 12:32:53 +0500

    Made active button work

--- todo-app-main/script.js
+++ todo-app-main/script.js
@@ -3,8 +3,12 @@ const ul = document.querySelector('ul');
 const todoList = document.querySelector('.todo-list');
 const addBtn = document.querySelector('.addBtn');
 const itemsLeft = document.querySelector('.no-of-items');
+const activeBtn = document.querySelector('.activeBtn');
+const all = document.querySelector('.all');
+const completed = document.querySelector('.completed');
 
 let todos = [];
+let checkedTodos = [];
 
 input.addEventListener('keypress', (e) => {
   if (e.key === 'Enter') {
@@ -22,6 +26,14 @@ input.addEventListener('keypress', (e) => {
       checkBox.classList.add('checkBox');
       checkBox.addEventListener('click', () => {
         checkBox.classList.add('checkBox-active');
+        if (checkBox.checked) {
+          // Add the checked li element to the checkedTodos array
+          checkedTodos.push(li);
+        } else {
+          // Remove the unchecked li element from the checkedTodos array
+          checkedTodos = checkedTodos.filter((todo) => todo !== li);
+        }
+        console.log(checkedTodos);
       });
 
       const p = document.createElement('p');
@@ -77,4 +89,53 @@ input.addEventListener('keypress', (e) => {
       });
     }
   }
+});
+
+// Add an event listener to the activeBtn
+activeBtn.addEventListener('click', () => {
+  document.querySelector('.active').classList.remove('active');
+  activeBtn.classList.add('active');
+  // Iterate through the checkedTodos array and remove each checked li element
+  checkedTodos.forEach((checkedLi) => {
+    checkedLi.remove();
+
+    // Remove the corresponding item from the todos and checkedTodos arrays
+    todos = todos.filter((todo) => todo !== checkedLi.textContent);
+    checkedTodos = checkedTodos.filter((checkedTodo) => checkedTodo !== checkedLi);
+  });
+
+  // Clear the checkedTodos array
+});
+
+// Add an event listener to the "All" button
+all.addEventListener('click', () => {
+  document.querySelector('.active').classList.remove('active');
+  all.classList.add('active');
+  
+  // Show all li elements
+  todos.forEach((todo) => {
+    const liElement = ul.querySelector(`li:contains("${todo}")`);
+    if (liElement) {
+      liElement.style.display = 'block';
+    }
+  });
+});
+
+// Add an event listener to the "Completed" button
+completed.addEventListener('click', () => {
+  document.querySelector('.active').classList.remove('active');
+  completed.classList.add('active');
+  
+  // Hide all li elements
+  todos.forEach((todo) => {
+    const liElement = ul.querySelector(`li:contains("${todo}")`);
+    if (liElement) {
+      liElement.style.display = 'none';
+    }
+  });
+  
+  // Show only checked li elements
+  checkedTodos.forEach((checkedLi) => {
+    checkedLi.style.display = 'block';
+  });
 });
