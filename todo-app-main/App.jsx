d947f89719de1642c6e53cd650229abcb0177aee
Muhammad Bilal <beastop975@gmail.com>
2023-10-23 12:32:31 +0500

    Creating react version

---
+++ todo-app-main/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/Header";
+import './index.css'
+
+function App() {
+  return (
+    <div className="container">
+    <Header/>
+    </div>
+  )
+}
+
+export default App
---
+++ todo-app-main/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='header'>
+      <div className='logo'>
+        TODO
+      </div>
+      <div className='light-dark'>
+        img
+      </div>
+    </div>
+  )
+}
